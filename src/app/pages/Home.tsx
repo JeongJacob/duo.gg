5748fd7c71bd7255a0c1c8afa08c4207e115dc98
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-08-14 20:45:52 +0900

    Design: Logo 삽입

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,3 +1,19 @@
+import home from "@/app/styles/Home.module.css";
+import Image from "next/image";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className={home.wrapper}>
+      <div className={home.logo__container}>
+        <Image
+          className={home.logo__image}
+          src={"/Logo_White.png"}
+          width={100}
+          height={100}
+          alt="duo.gg"
+        />
+      </div>
+      <div className={home.search__container}></div>
+    </div>
+  );
 }
