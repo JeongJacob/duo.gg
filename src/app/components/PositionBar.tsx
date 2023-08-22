cf2d4aaed4cb34e6374c5c82e7b9804c5ea5d3e9
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-08-22 23:27:25 +0900

    Feat: PositionBar 상태관리로 수정

--- src/app/components/PositionBar.tsx
+++ src/app/components/PositionBar.tsx
@@ -1,9 +1,19 @@
+"use client";
 import Image from "next/image";
 import { styled } from "styled-components";
-import lol from "@/app/styles/_LOL.module.css";
 import { InteractBtn } from "../duo/lol/page";
+import { useState } from "react";
+import lol from "@/app/styles/_LOL.module.css";
 
-const PositonBtn = styled.button`
+interface PositionT {
+  position: string;
+  num: number;
+}
+
+const PositonBtn = styled.button<{
+  selected: number;
+  idx: number;
+}>`
   width: 40px;
   height: 40px;
   display: flex;
@@ -12,65 +22,73 @@ const PositonBtn = styled.button`
   border-width: 1px 0px 1px 1px;
   border-style: solid;
   border-color: rgb(91, 91, 91);
+  background-color: ${(props) =>
+    props.selected === props.idx ? "rgb(79, 79, 79)" : ""};
   &:hover {
     background-color: rgb(79, 79, 79);
   }
 `;
 
+const positionBar: PositionT[] = [
+  {
+    position: "all",
+    num: 0,
+  },
+  {
+    position: "top",
+    num: 1,
+  },
+  {
+    position: "jug",
+    num: 2,
+  },
+  {
+    position: "mid",
+    num: 3,
+  },
+  {
+    position: "adc",
+    num: 4,
+  },
+  {
+    position: "sup",
+    num: 5,
+  },
+];
 export default function PositionBar() {
+  const [positionSelected, setPositionSelected] = useState(0);
+
+  const handleSelectedPosition = (
+    e: React.MouseEvent<HTMLButtonElement>,
+    position: number
+  ) => {
+    e.preventDefault();
+    setPositionSelected(position);
+  };
+
   return (
     <>
       <div className={lol.position__wrapper}>
-        <div className={lol.position__container}>
-          <PositonBtn>
-            <Image
-              src={"/position/all_icon.svg"}
-              width={20}
-              height={20}
-              alt="adc"
-            ></Image>
-          </PositonBtn>
-          <PositonBtn>
-            <Image
-              src={"/position/top_icon.svg"}
-              width={20}
-              height={20}
-              alt="adc"
-            ></Image>
-          </PositonBtn>
-          <PositonBtn>
-            <Image
-              src={"/position/jug_icon.svg"}
-              width={20}
-              height={20}
-              alt="adc"
-            ></Image>
-          </PositonBtn>
-          <PositonBtn>
-            <Image
-              src={"/position/mid_icon.svg"}
-              width={20}
-              height={20}
-              alt="adc"
-            ></Image>
-          </PositonBtn>
-          <PositonBtn>
-            <Image
-              src={"/position/adc_icon.svg"}
-              width={20}
-              height={20}
-              alt="adc"
-            ></Image>
-          </PositonBtn>
-          <PositonBtn>
-            <Image
-              src={"/position/sup_icon.svg"}
-              width={20}
-              height={20}
-              alt="adc"
-            ></Image>
-          </PositonBtn>
-        </div>
+        <ul className={lol.position__container}>
+          {positionBar.map((position) => (
+            <li key={position.num}>
+              <PositonBtn
+                selected={positionSelected}
+                idx={position.num}
+                onClick={(e) => {
+                  handleSelectedPosition(e, position.num);
+                }}
+              >
+                <Image
+                  src={`/position/${position.position}_icon.svg`}
+                  width={20}
+                  height={20}
+                  alt="adc"
+                />
+              </PositonBtn>
+            </li>
+          ))}
+        </ul>
         <InteractBtn width="100px">글 쓰기</InteractBtn>
       </div>
     </>
