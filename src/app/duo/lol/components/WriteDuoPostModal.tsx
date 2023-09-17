f03ec6e0b4d1df636c13bf0ea4b339261d5e88ae
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-09-17 21:21:26 +0900

    Feat: WriteDuoPostModal 컴포넌트 구현

---
+++ src/app/duo/lol/components/WriteDuoPostModal.tsx
@@ -0,0 +1,84 @@
+import DuoSelectModal from "@/app/components/DuoSelect";
+import { RootState } from "@/redux/store";
+import { Dispatch, SetStateAction, useState } from "react";
+import { IoMic } from "react-icons/io5";
+import { useSelector } from "react-redux";
+import PositionBar from "@/app/components/PositionBar";
+import InteractBtn from "@/app/components/InteractBtn";
+import { queueOptionData } from "../page";
+import { styled } from "styled-components";
+import lol from "@/app/styles/_LOL.module.css";
+
+const ToggleContainer = styled.div<{ $isToggle: boolean }>`
+  display: flex;
+  align-items: center;
+  width: 50px;
+  height: 27px;
+  background-color: ${(props) =>
+    props.$isToggle ? "rgb(103, 207, 74)" : "rgb(177, 177, 177)"};
+  border-radius: 50px;
+`;
+
+const ToggleCircle = styled.button<{ $isToggle: boolean }>`
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  background-color: white;
+  transition: 0.5s;
+  transform: ${(props) =>
+    props.$isToggle ? "translateX(22px)" : "translateX(3px)"};
+`;
+
+export default function WriteDuoPostModal({
+  setIsWriteModal,
+}: {
+  setIsWriteModal: Dispatch<SetStateAction<boolean>>;
+}) {
+  const [isToggle, setIsToggle] = useState(false);
+  const queueValue = useSelector(
+    (state: RootState) => state.selectTab.queueValue
+  );
+  return (
+    <div className={lol.duoPostModal__bg__wrapper}>
+      <div className={lol.duoPostModal__wrapper}>
+        <div>
+          <DuoSelectModal
+            selectData={queueOptionData}
+            width="180px"
+            isQueue={true}
+            defaultValue={queueValue}
+          />
+        </div>
+        <div className={lol.summonerName__container}>
+          <input type="text" placeholder="내 소환사명"></input>
+        </div>
+        <div className={lol.myPosition__container}>
+          내 포지션
+          <PositionBar />
+        </div>
+        <div className={lol.yourPosition__container}>
+          찾고 있는 포지션
+          <PositionBar />
+        </div>
+        <div className={lol.summonerBoard__container}>
+          <textarea placeholder="작성할 메모" />
+        </div>
+        <div className={lol.isVoice__container}>
+          <IoMic size="20px" />
+          <ToggleContainer $isToggle={isToggle}>
+            <ToggleCircle
+              $isToggle={isToggle}
+              onClick={() => setIsToggle(!isToggle)}
+            />
+          </ToggleContainer>
+        </div>
+        <InteractBtn width="100%" text={"등록하기"} />
+        <InteractBtn
+          width="100%"
+          text={"닫기"}
+          onClick={() => setIsWriteModal(false)}
+        />
+      </div>
+    </div>
+  );
+}
--- src/app/duo/lol/page.tsx
+++ src/app/duo/lol/page.tsx
@@ -1,25 +1,15 @@
 "use client";
-import React from "react";
+import { useState } from "react";
 import DuoPost from "@/app/components/DuoPost";
 import DuoSelect from "@/app/components/DuoSelect";
 import PositionBar from "@/app/components/PositionBar";
-import { styled } from "styled-components";
-import lol from "@/app/styles/_LOL.module.css";
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
+import InteractBtn from "@/app/components/InteractBtn";
+import lol from "@/app/styles/_LOL.module.css";
+import WriteDuoPostModal from "./components/WriteDuoPostModal";
 
-export const InteractBtn = styled.button<{ width: string }>`
-  background-color: #25ae25;
-  width: ${(props) => props.width};
-  height: 38px;
-  border-radius: 5px;
-
-  &:hover {
-    background-color: #186c18;
-  }
-`;
-
-const queueOptionData = [
+export const queueOptionData = [
   { value: "모든 큐", label: "모든 큐" },
   { value: "솔로랭크", label: "솔로랭크" },
   { value: "자유랭크", label: "자유랭크" },
@@ -38,6 +28,8 @@ const tierOptionsData = [
 ];
 
 function _LOL() {
+  const [isWriteModal, setIsWriteModal] = useState(false);
+
   const queueValue = useSelector(
     (state: RootState) => state.selectTab.queueValue
   );
@@ -45,24 +37,34 @@ function _LOL() {
     (state: RootState) => state.selectTab.tierValue
   );
   return (
-    <div className={lol.wrapper}>
-      <div className={lol.type__wrapper}>
-        <DuoSelect
-          selectData={queueOptionData}
-          width="180px"
-          isQueue={true}
-          defaultValue={queueValue}
-        />
-        <DuoSelect
-          selectData={tierOptionsData}
-          width="160px"
-          isQueue={false}
-          defaultValue={tierValue}
-        />
+    <>
+      {isWriteModal && <WriteDuoPostModal setIsWriteModal={setIsWriteModal} />}
+      <div className={lol.wrapper}>
+        <div className={lol.type__wrapper}>
+          <DuoSelect
+            selectData={queueOptionData}
+            width="180px"
+            isQueue={true}
+            defaultValue={queueValue}
+          />
+          <DuoSelect
+            selectData={tierOptionsData}
+            width="160px"
+            isQueue={false}
+            defaultValue={tierValue}
+          />
+        </div>
+        <div className={lol.position__write__wrapper}>
+          <PositionBar />
+          <InteractBtn
+            width="100px"
+            text={"글 쓰기"}
+            onClick={() => setIsWriteModal(true)}
+          />
+        </div>
+        <DuoPost />
       </div>
-      <PositionBar />
-      <DuoPost />
-    </div>
+    </>
   );
 }
 
