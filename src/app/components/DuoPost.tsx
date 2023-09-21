f533106ca667a3e40a278292bfeb297ee2401da1
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-09-22 01:49:44 +0900

    Design: DuoPost 컴포넌트 레이아웃 수정

--- src/app/components/DuoPost.tsx
+++ src/app/components/DuoPost.tsx
@@ -5,68 +5,52 @@ import post from "@/app/styles/DuoPost.module.css";
 import { LOLDuoPostType } from "../duo/lol/page";
 
 export default function DuoPost({ postData }: { postData: LOLDuoPostType }) {
+
   return (
     <div className={post.wrapper}>
       <div className={post.container}>
-        <div className={post.user__info__container}>
+        <div className={post.summoner__info__container}>
           <Image
-            className={post.userImage__element}
+            className={post.summonerImage__element}
             src={"/challenger.webp"}
             width={50}
             height={50}
             alt="adc"
           />
           <div>
-            <div className={post.user__name__container}>
-              <Image
-                src={`/position/${postData.myPositonValue}_icon.svg`}
-                width={20}
-                height={20}
-                alt="adc"
-              />
-              <span className={post.user__name__element}>
-                {postData.summonerName}
-              </span>
+            <div className={post.summoner__title__container}>
+              {postData.queueValue}&nbsp;
+              {postData.yourPositonValue}&nbsp;구합니다.
             </div>
-            <div className={post.user__info__tier__container}>
+            <div className={post.summoner__info__tier__container}>
               <Image
-                className={post.user__info__tier}
+                className={post.summoner__info__tier}
                 src={"/challenger.webp"}
                 width={30}
                 height={30}
                 alt="adc"
               />
               <span>C1</span>
-              {postData.isVoice && <IoMic className={post.user__info__voice} />}
+              {postData.isVoice && (
+                <IoMic className={post.summoner__info__voice} />
+              )}
             </div>
           </div>
         </div>
-        <div className={post.user__memo__container}>
+        <div className={post.summoner__board__container}>
           {postData.summonerBoard}
         </div>
-        <div className={post.user__record__container}>
-          <div className={post.user__most__container}>
+        <div className={post.summoner__record__container}>
+          <div className={post.summoner__name__container}>
             <Image
-              className={post.userImage__element}
-              src={"/challenger.webp"}
-              width={40}
-              height={40}
+              src={`/position/${postData.myPositonValue}_icon.svg`}
+              width={20}
+              height={20}
               alt="adc"
             />
-            <Image
-              className={post.userImage__element}
-              src={"/challenger.webp"}
-              width={40}
-              height={40}
-              alt="adc"
-            />
-            <Image
-              className={post.userImage__element}
-              src={"/challenger.webp"}
-              width={40}
-              height={40}
-              alt="adc"
-            />
+            <span className={post.summoner__name__element}>
+              {postData.summonerName}
+            </span>
           </div>
           <InteractBtn width="120px" text={"전적 보기"} />
         </div>
