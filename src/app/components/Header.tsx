01d229d5803afdb609eaad7c1eda0b3abae21761
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-10-06 23:29:54 +0900

    Fix: 로그인 여부 조건문 수정

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
       <div className={header.container}>
         <Link href={"/"}>홈</Link>
         <Link href={"/duo/lol"}>듀오찾기</Link>
-        {isLogin ? (
+        {!isLogin ? (
           <Link href={"/login"}>로그인</Link>
         ) : (
           <>
