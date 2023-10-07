19f83e3507eebf15d2b184f4adba9d8c2da5a494
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-10-07 13:56:29 +0900

    Feat: SingUpModal input onChange 함수 기능 구현

--- src/app/login/components/SignUpModal.tsx
+++ src/app/login/components/SignUpModal.tsx
@@ -2,14 +2,34 @@
 import { LoginDiv, LoginInput } from "../page";
 import InteractBtn from "@/app/components/InteractBtn";
 import Image from "next/image";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import login from "@/app/styles/Login.module.css";
+
+interface SignUpInputT {
+  email: string;
+  password: string;
+  repassword: string;
+  nickname: string;
+}
 
 export default function SignUpModal({
   setIsSignUp,
 }: {
   setIsSignUp: Dispatch<SetStateAction<boolean>>;
 }) {
+  const [signUpInputText, setSignUpInputText] = useState<SignUpInputT>({
+    email: "",
+    password: "",
+    repassword: "",
+    nickname: "",
+  });
+  const onChangeSignUpInputText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target;
+    setSignUpInputText({
+      ...signUpInputText,
+      [name]: value,
+    });
+  };
   return (
     <div className={login.modal__bg__wrapper}>
       <div className={login.modal__wrapper}>
@@ -22,22 +42,41 @@ export default function SignUpModal({
               alt="duo.gg"
             />
           </div>
-          <LoginDiv>
-            <span>이메일</span>
-            <LoginInput type="email" placeholder="jacob@duo.gg" />
-          </LoginDiv>
-          <LoginDiv>
-            <span>비밀번호</span>
-            <LoginInput type="password" />
-          </LoginDiv>
-          <LoginDiv>
-            <span>비밀번호 확인</span>
-            <LoginInput type="password" />
-          </LoginDiv>
-          <LoginDiv>
-            <span>닉네임</span>
-            <LoginInput type="text" />
-          </LoginDiv>
+          <form>
+            <LoginDiv>
+              <span>이메일</span>
+              <LoginInput
+                type="email"
+                placeholder="jacob@duo.gg"
+                required
+                onChange={(e) => onChangeSignUpInputText(e)}
+              />
+            </LoginDiv>
+            <LoginDiv>
+              <span>비밀번호</span>
+              <LoginInput
+                type="password"
+                required
+                onChange={(e) => onChangeSignUpInputText(e)}
+              />
+            </LoginDiv>
+            <LoginDiv>
+              <span>비밀번호 확인</span>
+              <LoginInput
+                type="password"
+                required
+                onChange={(e) => onChangeSignUpInputText(e)}
+              />
+            </LoginDiv>
+            <LoginDiv>
+              <span>닉네임</span>
+              <LoginInput
+                type="text"
+                required
+                onChange={(e) => onChangeSignUpInputText(e)}
+              />
+            </LoginDiv>
+          </form>
           <div className={login.btn__container}>
             <InteractBtn width="100px" text="회원가입" />
             <InteractBtn
