89c9ed9ced0da0f4b902ad1e4b9e97e94a36dc98
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-10-13 21:41:27 +0900

    Design: SignUp ErrorMsg 디자인 구현

--- src/app/login/components/SignUpModal.tsx
+++ src/app/login/components/SignUpModal.tsx
@@ -4,13 +4,42 @@ import InteractBtn from "@/app/components/InteractBtn";
 import Image from "next/image";
 import { Dispatch, SetStateAction, useState } from "react";
 import login from "@/app/styles/Login.module.css";
+import styled from "styled-components";
+import { AiFillWarning } from "react-icons/ai";
 
 interface SignUpInputT {
   email: string;
   password: string;
-  repassword: string;
+  passwordValid: string;
   nickname: string;
 }
+
+interface SignUpValidT {
+  isEmailValid: boolean;
+  isPasswordValid: boolean;
+  isConfirmPasswordValid: boolean;
+  isNicknameValid: boolean;
+}
+const InvalidSignUpInputMsg = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  margin-top: 0.3rem;
+  color: rgb(255, 225, 0);
+  animation-name: vibrate;
+  animation-duration: 0.1s;
+  animation-iteration-count: 4;
+  font-size: 12px;
+
+  @keyframes vibrate {
+    from {
+      padding-left: 0.7rem;
+    }
+    to {
+      padding-right: 0.3rem;
+    }
+  }
+`;
 
 export default function SignUpModal({
   setIsSignUp,
@@ -20,16 +49,10 @@ export default function SignUpModal({
   const [signUpInputText, setSignUpInputText] = useState<SignUpInputT>({
     email: "",
     password: "",
-    repassword: "",
+    passwordValid: "",
     nickname: "",
   });
-  const onChangeSignUpInputText = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value, name } = e.target;
-    setSignUpInputText({
-      ...signUpInputText,
-      [name]: value,
-    });
-  };
+
   return (
     <div className={login.modal__bg__wrapper}>
       <div className={login.modal__wrapper}>
@@ -45,46 +68,29 @@ export default function SignUpModal({
           <form>
             <LoginDiv>
               <span>이메일</span>
-              <LoginInput
-                type="email"
-                placeholder="jacob@duo.gg"
-                required
-                onChange={(e) => onChangeSignUpInputText(e)}
-              />
+              <LoginInput type="email" placeholder="jacob@duo.gg" required />
             </LoginDiv>
             <LoginDiv>
               <span>비밀번호</span>
-              <LoginInput
-                type="password"
-                required
-                onChange={(e) => onChangeSignUpInputText(e)}
-              />
+              <LoginInput type="password" required />
             </LoginDiv>
             <LoginDiv>
               <span>비밀번호 확인</span>
-              <LoginInput
-                type="password"
-                required
-                onChange={(e) => onChangeSignUpInputText(e)}
-              />
+              <LoginInput type="password" required />
             </LoginDiv>
             <LoginDiv>
               <span>닉네임</span>
-              <LoginInput
-                type="text"
-                required
-                onChange={(e) => onChangeSignUpInputText(e)}
-              />
+              <LoginInput type="text" required />
             </LoginDiv>
+            <div className={login.btn__container}>
+              <InteractBtn width="100px" text="회원가입" type="submit" />
+              <InteractBtn
+                width="100px"
+                text="돌아가기"
+                onClick={() => setIsSignUp(false)}
+              />
+            </div>
           </form>
-          <div className={login.btn__container}>
-            <InteractBtn width="100px" text="회원가입" />
-            <InteractBtn
-              width="100px"
-              text="돌아가기"
-              onClick={() => setIsSignUp(false)}
-            />
-          </div>
         </div>
       </div>
     </div>
